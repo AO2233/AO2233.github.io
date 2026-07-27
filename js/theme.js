9e577c6f828d0040e23175cf74c7a31d684f96e1
AO2233 <ao.debian@gmail.com>
2026-07-28 02:10:29 +0900

    auto commit

--- js/theme.js
+++ js/theme.js
@@ -188,6 +188,8 @@ function initMath() {
   renderMathInElement(document.body, {
     delimiters: [
       { left: "$$", right: "$$", display: true },
+      { left: "\\[", right: "\\]", display: true },
+      { left: "\\(", right: "\\)", display: false },
       { left: "$", right: "$", display: false },
     ],
   });
